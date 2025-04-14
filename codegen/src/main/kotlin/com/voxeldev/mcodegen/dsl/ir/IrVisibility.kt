6a32a9dab3d667fccbc5fa9aefb8ea889e1d51dd
nVoxel <nvoxel@outlook.com>
2025-04-15 01:54:39 +0300

    Open IrVisibility class

--- codegen/src/main/kotlin/com/voxeldev/mcodegen/dsl/ir/IrVisibility.kt
+++ codegen/src/main/kotlin/com/voxeldev/mcodegen/dsl/ir/IrVisibility.kt
@@ -4,6 +4,30 @@ package com.voxeldev.mcodegen.dsl.ir
  * Represents the visibility modifiers in the IR (Intermediate Representation) system.
  * This enum defines the different levels of visibility that can be applied to code elements.
  */
-enum class IrVisibility {
-    PUBLIC, PROTECTED, INTERNAL, PRIVATE
-}
+open class IrVisibility(
+    open val stringRepresentation: IrStringRepresentation,
+)
+
+data class IrVisibilityPublic(
+    override val stringRepresentation: IrStringRepresentation,
+) : IrVisibility(
+    stringRepresentation = stringRepresentation,
+)
+
+data class IrVisibilityProtected(
+    override val stringRepresentation: IrStringRepresentation,
+) : IrVisibility(
+    stringRepresentation = stringRepresentation,
+)
+
+data class IrVisibilityInternal(
+    override val stringRepresentation: IrStringRepresentation,
+) : IrVisibility(
+    stringRepresentation = stringRepresentation,
+)
+
+data class IrVisibilityPrivate(
+    override val stringRepresentation: IrStringRepresentation,
+) : IrVisibility(
+    stringRepresentation = stringRepresentation,
+)
